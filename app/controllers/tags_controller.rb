f9207558a59864eb76a63d5c6a790efc714f2585
yan_runchen <runchen.yan@drecom.co.jp>
2013-12-21 23:33:37 +0900

    demo ver1.0

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,7 +5,8 @@ class TagsController < ApplicationController
   # GET /tags
   # GET /tags.json
   def index
-    @tags = Tag.all
+    #@tags = Tag.all
+    @tags = Tag.where("(id>=14 AND id<=24) OR id=52")
   end
 
   # GET /tags/1
